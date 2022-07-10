290afa6782a1275da1de29c3b09cce0e5ee1e310
Tom Chataway <pugtura@gmail.com>
2022-07-10 16:44:50 +0930

    Refactor ability get or guess logic to utilities

--- damage_calculator_format_pokemon.py
+++ damage_calculator_format_pokemon.py
@@ -1,7 +1,6 @@
 from poke_env.data import POKEDEX
 from poke_env.environment.pokemon import Pokemon
-from ability_dex import AbilityDex
-import random
+from utils import UtilityFunctions
 
 class DamageCalculatorFormatPokemon():
     def __init__(
@@ -11,19 +10,9 @@ class DamageCalculatorFormatPokemon():
         pokedex_entry = POKEDEX[pokemon.species]
         print(pokedex_entry)
         self.species = pokedex_entry.get('name')
-        poss_abilities = list(pokedex_entry.get('abilities').values())
-
-        if pokemon.ability is None:
-            # Don't know ability, so randomly pick one from possible options.
-            print(poss_abilities)
-            random_ability = random.choice(poss_abilities)
-            print("Randomly selected " + random_ability)
-            self.ability = random_ability
-        else:
-            # poke_env Pokemon have condensed ability IDs, but the damage calculator
-            # needs the verbose version, so convert that here.
-            ability_dex = AbilityDex()
-            self.ability = ability_dex.get_ability(pokemon.ability)
+        
+        utils = UtilityFunctions()
+        self.ability = utils.get_or_guess_ability(pokemon)
 
         if pokemon.item is None:
             # API requires a non-null value for hold item.
@@ -31,13 +20,13 @@ class DamageCalculatorFormatPokemon():
         else:
             self.item = pokemon.item
 
-        self.level = 50
+        self.level = pokemon.level
 
     def formatted(self):
         return {
             "species": self.species, #species name AS IT IS IN THE POKEDEX  [REQUIRED]
             "ability": self.ability, #ability [REQUIRED]
             "item": self.item,  #item [REQUIRED]
-            "level": 50, #level [REQUIRED], must be a number
+            "level": self.level, #level [REQUIRED], must be a number
         }
 
--- utils.py
+++ utils.py
@@ -1,3 +1,8 @@
+from poke_env.data import POKEDEX
+from poke_env.environment.pokemon import Pokemon
+from ability_dex import AbilityDex
+import random
+
 class UtilityFunctions():
     def move_targets_single_pokemon(self, move_target: str):
         match move_target:
@@ -19,3 +24,21 @@ class UtilityFunctions():
                 return True
 
         return False
+
+    def get_or_guess_ability(self, pokemon):
+        pokedex_entry = POKEDEX[pokemon.species]
+        poss_abilities = list(pokedex_entry.get('abilities').values())
+        ability = None
+
+        if pokemon.ability is None:
+            # Don't know ability, so randomly pick one from possible options for
+            # species.
+            random_ability = random.choice(poss_abilities)
+            ability = random_ability
+        else:
+            # poke_env Pokemon have condensed ability IDs, but the damage
+            # calculator needs the verbose version, so convert that here.
+            ability_dex = AbilityDex()
+            ability = ability_dex.get_ability(pokemon.ability)
+
+        return ability
